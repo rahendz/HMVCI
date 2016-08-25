bc747f473c2e654b2c20b16c07abcd4f4720db7a
Rahendra Putra K <rahendz@gmail.com>
2016-08-25 12:59:24 +0700

    removing unnecessary variables

--- application/includes/controller/private.php
+++ application/includes/controller/private.php
@@ -2,30 +2,11 @@
 
 abstract class Private_Controller extends Base_Controller {
 
-	protected $theme = 'admin';
-	protected $theme_part = NULL;
-	protected $data_theme = array();
-	protected $content_theme = NULL;
-	protected $enqueue_style = array();
-	protected $enqueue_script = array();
+	protected $theme_var = array('config'=>array('backend'=>'default'));
 
 	public function __construct() {
 		parent::__construct();
 		// $this->user_priviledge();
-	}
-
-	protected function render_theme ( $vars = array(), $return = FALSE ) {
-		$this->load->theme_config['backend'] = $this->theme;
-		$this->load->enqueue_style = $this->enqueue_style;
-		$this->load->enqueue_script = $this->enqueue_script;
-		$this->load->views_data = $this->data_theme;
-		$this->load->views_file = $this->content_theme;
-		$this->load->theme_part = $this->theme_part;
-		return $this->load->theme ( $vars, $return );
-	}
-
-	protected function views ( $path, $vars = array(), $return = FALSE ) {
-		return $this->load->view ( $path, $vars, $return );
 	}
 
 	protected function user_priviledge() {
--- application/includes/controller/public.php
+++ application/includes/controller/public.php
@@ -1,13 +1,7 @@
 <?php if ( ! defined ( 'BASEPATH' ) ) exit ( 'No direct script access allowed' );
 
 abstract class Public_Controller extends Base_Controller {
-
-	// protected $theme = 'default';
-	// protected $theme_part = null;
-	// protected $data_theme = array();
-	// protected $content_theme = null;
-	// protected $enqueue_style = array();
-	// protected $enqueue_script = array();
+	protected $theme_var = array('config'=>array('frontend'=>'default'));
 
 	public function __construct() {
 		parent::__construct();
