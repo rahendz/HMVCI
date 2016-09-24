08950b9997bf6bb2685a27c8e498fe85bd79dae3
Rahendra Putra K <rahendz@gmail.com>
2016-09-24 19:30:56 +0700

    merging from hmvci2

--- application/includes/controller/public.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined ( 'BASEPATH' ) ) exit ( 'No direct script access allowed' );
-
-abstract class Public_Controller extends Base_Controller {
-	protected $theme_var = array('config'=>array('frontend'=>'default'));
-
-	public function __construct() {
-		parent::__construct();
-	}
-}
