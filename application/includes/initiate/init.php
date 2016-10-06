8e11c90acdcd684a36d2938a1bc30252f201c1e4
Rahendra Putra K <rahendz@gmail.com>
2016-10-06 18:53:44 +0700

    refine code at init file

--- application/includes/initiate/init.php
+++ application/includes/initiate/init.php
@@ -1,16 +1,14 @@
 <?php if ( ! defined ( 'BASEPATH' ) ) exit ( 'No direct script access allowed' );
 
-@require_once dirname ( __FILE__ ) .'/defines.php';
-@require_once dirname ( __FILE__ ) .'/unit_test.php';
-@require_once dirname ( __FILE__ ) .'/assign.php';
-@require_once dirname ( __FILE__ ) .'/routing.php';
+$init_files = array('defines', 'unit_test', 'assign', 'routing');
+foreach ($init_files as $file) { @require_once dirname(__FILE__)."/{$file}.php"; }
 
 $root_files = array ( 'config' );
 $core_files = array ( 'functions', 'themes', 'auth', 'format', 'controller', 'rest', 'model' );
 $base_files = array ( 'private', 'public', 'api' );
 
-if ( isset ( $my_config ) AND count ( $my_config ) > 0 ) {
-	foreach ( $my_config as $c => $i ) {
+if (isset($my_config) && count($my_config)>0) {
+	foreach ($my_config as $c => $i) {
 		$assign_to_config[$c] = $i;
 	}
 }
