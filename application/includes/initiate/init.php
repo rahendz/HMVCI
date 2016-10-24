6ac33e241536d87740cc0ccfdb1342f211ab15e5
Rahendra Putra K <rahendz@gmail.com>
2016-10-24 14:46:09 +0700

    refine code, remove whitespace

--- application/includes/initiate/init.php
+++ application/includes/initiate/init.php
@@ -3,9 +3,9 @@
 $init_files = array('defines', 'unit_test', 'assign', 'routing');
 foreach ($init_files as $file) { @require_once dirname(__FILE__)."/{$file}.php"; }
 
-$root_files = array ( 'config' );
-$core_files = array ( 'functions', 'themes', 'auth', 'format', 'controller', 'rest', 'model' );
-$base_files = array ( 'private', 'public', 'api' );
+$root_files = array('config');
+$core_files = array('functions', 'themes', 'auth', 'format', 'controller', 'rest', 'model');
+$base_files = array('private', 'public', 'api');
 
 if (isset($my_config) && count($my_config)>0) {
 	foreach ($my_config as $c => $i) {
