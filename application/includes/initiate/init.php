5f23c9e75a8b95db7a72af8019d321d35c9cbc6b
Rahendra Putra K <rahendz@gmail.com>
2016-10-25 10:45:12 +0700

    re-arrange loaded file, load function back before themes function

--- application/includes/initiate/init.php
+++ application/includes/initiate/init.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined ( 'BASEPATH' ) ) exit ( 'No direct script access allowed' );
 
-$init_files = array('defines', 'unit_test', 'functions', 'assign', 'routing');
+$init_files = array('defines', 'unit_test', 'assign', 'routing');
 foreach ($init_files as $file) { @require_once dirname(__FILE__)."/{$file}.php"; }
 
 $root_files = array('config');
-$core_files = array('themes', 'auth', 'format', 'controller', 'rest', 'model');
+$core_files = array('functions', 'themes', 'auth', 'format', 'controller', 'rest', 'model');
 $base_files = array('private', 'public', 'api');
 
 if (isset($my_config) && count($my_config)>0) {
