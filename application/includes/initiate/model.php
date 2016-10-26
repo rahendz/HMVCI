75b568bf72a2f0adcc7d7ee3bdc1ee92b9641cbc
Rahendra Putra K <rahendz@gmail.com>
2016-10-26 19:21:07 +0700

    refixing logic request for handle better

--- application/includes/initiate/model.php
+++ application/includes/initiate/model.php
@@ -10,6 +10,7 @@ if (!class_exists('Base_Model')) {
 class Model extends Models {
 	public $initiate = false;
 	public $table = null;
+	public $result = false;
 	protected $_ci;
 	protected $db;
 
@@ -18,31 +19,37 @@ class Model extends Models {
 		if (!isset($this->db)) {
 			$this->_ci->load->database();
 			$this->db =& $this->_ci->db;
+			unset($this->_ci->db);
 		}
 		$this->db->save_queries = false;
-		$this->db->start_cache();
+		$this->__recall('start_cache');
 	}
 
-	public function __call($method, $args) {
-		$result = false;
+	public function __call ($method, $args) {
+		// $result = false;
 		$own_method = array('get_all', 'get_all_array', 'get_one', 'get_one_array', 'count_all', 'count_results', 'is_exists');
 		if (!$this->initiate) {
 			$this->initiate = true;
 			if ($method==='select') {
-				$this->db->select($args);
+				// $this->db->select($args);
+				$this->__recall('select',$args);
 			}
-			$this->db->from($this->table);
+			// $this->db->from($this->table);
+			$this->__recall('from',array($this->table));
 		}
 		if (in_array($method, $own_method)) {
-			$this->db->stop_cache();
-			$result = true;
+			// $this->db->stop_cache();
+			$this->__recall('stop_cache');
+			$this->result = true;
 			$this->initiate = false;
-			$query = $this->db->get();
-		} elseif ($method!='select') {
-			return call_user_func_array(array($this->db, $method), $args);
+			// $query = $this->db->get();
+			$query = $this->__recall('get');
+		} elseif (!in_array($method, $own_method) && $method!=='select') {
+			// $return = call_user_func_array(array($this->db, $method), $args);
+			$return = $this->__recall($method, $args);
 		}
 
-		if ($result) {
+		if ($this->result!==false) {
 			switch ($method) {
 				case 'get_all':
 					$return = $query->result();
@@ -57,21 +64,34 @@ class Model extends Models {
 					$return = $query->row_array();
 					break;
 				case 'count_all':
-					$return = $query->count_all();
+					// $return = $this->db->count_all();
+					$return = $this->__recall('count_all');
 					break;
 				case 'count_results':
-					$return = $query->count_all_results();
+					// $return = $query->count_all_results();
+					$return = $this->__recall('count_all_results');
 					break;
 				case 'is_exists':
-					$return = false;
+					$return = new stdClass();
+					$return->status = false;
 					if ($query->num_rows()>0) {
-						$return = true;
+						$return->status = true;
+						$return->get_all = $query->result();
+						$return->get_one = $query->row();
 					}
 					break;
 			}
+			// $this->db->flush_cache();
+			$this->__recall('flush_cache');
 		}
-		$this->db->flush_cache();
-		return $return;
+
+		if (isset($return)) {
+			return $return;
+		}
+	}
+
+	private function __recall ($method, $args=array()) {
+		return call_user_func_array(array($this->db, $method), $args);
 	}
 }
 
