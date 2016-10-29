cdc2fbd67b28073288d69f7368418df3604dd7f9
Rahendra Putra K <rahendz@gmail.com>
2016-10-29 12:58:06 +0700

    adding last_query and num_row variable on each request for better handle

--- application/includes/initiate/model.php
+++ application/includes/initiate/model.php
@@ -11,6 +11,8 @@ class Model extends Models {
 	public $initiate = false;
 	public $table = null;
 	public $result = false;
+	public $num_rows = '0';
+	public $last_query = false;
 	protected $_ci;
 	protected $db;
 
@@ -26,28 +28,28 @@ class Model extends Models {
 	}
 
 	public function __call ($method, $args) {
-		// $result = false;
 		$own_method = array('get_all', 'get_all_array', 'get_one', 'get_one_array', 'count_all', 'count_results', 'is_exists');
+
 		if (!$this->initiate) {
 			$this->initiate = true;
 			if ($method==='select') {
-				// $this->db->select($args);
 				$this->__recall('select',$args);
 			}
-			// $this->db->from($this->table);
 			$this->__recall('from',array($this->table));
 		}
 		if (in_array($method, $own_method)) {
-			// $this->db->stop_cache();
 			$this->__recall('stop_cache');
 			$this->result = true;
 			$this->initiate = false;
-			// $query = $this->db->get();
 			$query = $this->__recall('get');
+			$this->num_rows = $query->num_rows();
 		} elseif (!in_array($method, $own_method) && $method!=='select') {
-			// $return = call_user_func_array(array($this->db, $method), $args);
 			$return = $this->__recall($method, $args);
+			if (in_array($method, array('insert', 'insert_batch', 'update', 'update_batch', 'delete', 'truncate'))) {
+				$this->__recall('flush_cache');
+			}
 		}
+		$this->last_query = $this->__recall('last_query');
 
 		if ($this->result!==false) {
 			switch ($method) {
@@ -64,11 +66,9 @@ class Model extends Models {
 					$return = $query->row_array();
 					break;
 				case 'count_all':
-					// $return = $this->db->count_all();
 					$return = $this->__recall('count_all');
 					break;
 				case 'count_results':
-					// $return = $query->count_all_results();
 					$return = $this->__recall('count_all_results');
 					break;
 				case 'is_exists':
@@ -81,10 +81,8 @@ class Model extends Models {
 					}
 					break;
 			}
-			// $this->db->flush_cache();
 			$this->__recall('flush_cache');
 		}
-
 		if (isset($return)) {
 			return $return;
 		}
