52f1969a01ee54f44cf96fdc31ecb9f409005c62
Rahendra Putra K <rahendz@gmail.com>
2017-01-23 18:51:21 +0700

    defining bcrypt pass constant

--- application/includes/initiate/defines.php
+++ application/includes/initiate/defines.php
@@ -9,3 +9,9 @@ define('LIMIT_SIZE', '1024M');
 
 // Switch 'true' if you wanna use REST Api
 define ( 'REST_API', false );
+
+if (!defined('PASSWORD_BCRYPT')) {
+	define('PASSWORD_BCRYPT', 1);
+	define('PASSWORD_DEFAULT', PASSWORD_BCRYPT);
+	define('PASSWORD_BCRYPT_DEFAULT_COST', 10);
+}
