205e000cbe84e10efe7f20a09fe858441d56b215
Rahendra Putra K™ <rahendz@gmail.com>
2019-07-10 02:58:53 +0700

    Small change

--- application/includes/initiate/init.php
+++ application/includes/initiate/init.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined ( 'BASEPATH' ) ) exit ( 'No direct script access allowed' );
 
 $init_files = array('defines', 'utilities', 'assign', 'routing');
-foreach ($init_files as $file) { @require_once dirname(__FILE__).DIRECTORY_SEPARATOR."{$file}.php"; }
+foreach ($init_files as $file) { @require_once dirname(__FILE__).DIRECTORY_SEPARATOR."{$file}".EXT; }
 
 $root_files = array('config');
 $core_files = array('functions', 'themes', 'auth', 'format', 'controller', 'rest', 'model');
